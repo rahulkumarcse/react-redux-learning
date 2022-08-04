137f90f323f1d62ee4fc58e52d704f05c564e9c6
Rahul Kumar <rahulcsofficial@gmail.com>
2022-08-04 22:27:28 +0530

    Redux Action creator and reducer added

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -4,10 +4,10 @@ import React from 'react'
 const Shop = () => {
   return (
     <div>
-            <h2>But this Shoes 50Rs</h2>
 
+        <h2>Deposit / Withdraw Money</h2>
       <button className='btn btn-primary mx-2'>-</button>
-      Add to cart
+      Update Balance
       <button className='btn btn-primary mx-2'>+</button>
 
     </div>
---
+++ src/state/reducers/amountReducer.js
@@ -0,0 +1,11 @@
+export default reducer = (state=0,action) =>{
+        if(action.type =="depost"){
+            return state+action.payload
+        }
+        else if(action.type =="withdraw"){
+                return state-action.payload
+        }
+        else{
+            return state
+        }
+}
