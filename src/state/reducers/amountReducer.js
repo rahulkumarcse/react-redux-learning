dd4c53498a4ed1f1af8f76220cc05d9c05feed88
Rahul Kumar <rahulcsofficial@gmail.com>
2022-08-04 23:34:33 +0530

    Redux store added

--- src/state/reducers/amountReducer.js
+++ src/state/reducers/amountReducer.js
@@ -1,4 +1,4 @@
-export default reducer = (state=0,action) =>{
+ const reducer = (state=0,action) =>{
         if(action.type =="depost"){
             return state+action.payload
         }
@@ -9,3 +9,4 @@ export default reducer = (state=0,action) =>{
             return state
         }
 }
+export default reducer
